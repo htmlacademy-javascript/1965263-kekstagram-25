f361ca07d5fbe43c93cd81121311e9c34cf7d2e1
Mikhail-Sakharov <sakharov_mikhail@hotmail.com>
2022-03-12 16:21:24 +0300

    Рефакторинг модуля отрисовки миниатюр

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
-import './data.js';
-import './render-thumbnails.js';
+import {fakeServerResponse} from './data.js';
+import {renderData} from './render-thumbnails.js';
+
+const data = fakeServerResponse();
+renderData(data);
--- js/render-thumbnails.js
+++ js/render-thumbnails.js
@@ -1,22 +1,23 @@
-import {fakeServerResponse} from './data.js';
+function renderData(data) {
+  const thumbnailsContainer = document.querySelector('.pictures');
+  const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  const tempContainer = document.createDocumentFragment();
 
-const thumbnailsData = fakeServerResponse();
-const thumbnailsContainer = document.querySelector('.pictures');
-const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const tempContainer = document.createDocumentFragment();
+  data.forEach((item) => {
+    const PictureInfo = {
+      URL: item.url,
+      LIKES: item.likes,
+      COMMENTS: item.comments.length
+    };
+    const thumbnailItem = thumbnailTemplate.cloneNode(true);
 
-thumbnailsData.forEach((item) => {
-  const PictureInfo = {
-    URL: item.url,
-    LIKES: item.likes,
-    COMMENTS: item.comments.length
-  };
-  const thumbnailItem = thumbnailTemplate.cloneNode(true);
+    thumbnailItem.querySelector('.picture__img').src = PictureInfo.URL;
+    thumbnailItem.querySelector('.picture__likes').textContent = PictureInfo.LIKES;
+    thumbnailItem.querySelector('.picture__comments').textContent = PictureInfo.COMMENTS;
+    tempContainer.append(thumbnailItem);
+  });
 
-  thumbnailItem.querySelector('.picture__img').src = PictureInfo.URL;
-  thumbnailItem.querySelector('.picture__likes').textContent = PictureInfo.LIKES;
-  thumbnailItem.querySelector('.picture__comments').textContent = PictureInfo.COMMENTS;
-  tempContainer.append(thumbnailItem);
-});
+  thumbnailsContainer.append(tempContainer);
+}
 
-thumbnailsContainer.append(tempContainer);
+export {renderData};
